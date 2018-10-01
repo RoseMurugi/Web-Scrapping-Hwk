893d524d009933068b1317d8e46abf3a20423126
Rose Wachira <waridicook@gmail.com>
2018-09-30 22:31:58 -0500

    hwk files

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
-# import the dependancies/libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, redirect
 import pymongo
-import scrape
+from flask_pymongo import PyMongo
+import scrape_mars
 
-# create instance of Flask app
 app = Flask(__name__)
 
-# create mongo connection
-client = pymongo.MongoClient()
-db = client.mars_db
-collection = db.mars_data_entries
+app.config["MONGO_URI"] = "mongodb://localhost:27017/scrape"
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def home():
-    mars_data = list(db.collection.find())[0]
-    return render_template('index.html', mars_data = mars_data)
-
+def index():
+    try:
+        mars_data = mongo.db.mars_data.find_one()
+        return render_template('index.html', mars_data=mars_data)
+    except:
+        return redirect("http://localhost:5000/scrape", code=302)
 
 @app.route("/scrape")
-def web_scrape():
-    db.collection.remove({})
-    mars_data = scrape.scrape()
-    return render_template('scrape.html')
+def scraped():
+    mars_data = mongo.db.mars_data
+    mars_data_scrape = scrape_mars.scrape()
+    mars_data.update(
+        {},
+        mars_data_scrape,
+        upsert=True
+    )
+    return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
-    app.run(debug =True)
+    app.run(debug=True)
+
+
